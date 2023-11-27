40beb82813240f4a8c5c1f86e95d96fae83281b7
unknown <elsie.dh@gmail.com>
2023-11-26 22:25:54 -0800

    added shape svg code

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 // Runs the application using imports from lib/
 
-const { default: inquirer } = require('inquirer')
+//const { default: inquirer } = require('inquirer')
 
 console.log('hello world!')
-
-inquirer = require('inquirer')
+inquire = require('inquirer')
 fs = require('fs')
-svgBuilder = require('svg-builder')
+//svgBuilder = require('svg-builder')
 
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
+// GIVEN a command-line application that accepts user input //
+// WHEN I am prompted for text //
+// THEN I can enter up to three characters //
+// WHEN I am prompted for the text color //
+// THEN I can enter a color keyword (OR a hexadecimal number) //
+// WHEN I am prompted for a shape //
+// THEN I am presented with a list of shapes to choose from: circle, triangle, and square //
+// WHEN I am prompted for the shape's color //
+// THEN I can enter a color keyword (OR a hexadecimal number) //
+// WHEN I have entered input for all the prompts  
 // THEN an SVG file is created named `logo.svg`
 // AND the output text "Generated logo.svg" is printed in the command line
 // WHEN I open the `logo.svg` file in a browser
 // THEN I am shown a 300x200 pixel image that matches the criteria I entered
 
 
-const questions = [
-    {
+const questions = 
+    [{
         type: 'input',
-        message: "Enter up to 3 characters for logo",
+        message: "Enter up to 3 characters for a logo:",
         name: 'text',
         default: '',
         validate: function (answer) {
             if (answer.length > 3) {
-                return console.log("You are limitted to three characters");
+                return console.log("\n ALERT: You are limitted to three characters, please try again");
             }
             return true;
         }
     },
     {
         type: 'input',
-        message: "Indiacte text color (keyword or hexidecimal number)",
-        name: 'text-color',
+        message: "Indiacte a prefered logo text color (keyword or hexidecimal number):",
+        name: 'textColor',
         default: '',
         validate: function (answer) {
             if (answer.length < 1) {
@@ -52,14 +51,14 @@ const questions = [
     },
     {
         type: 'list',
-        message: "Chose shape:",
+        message: "Chose shape from the available shapes:",
         name: 'shape',
-        choices: ['circle', 'triangle', 'square']
+        choices: ['Circle', 'Triangle', 'Square']
     }, 
     {
         type: 'input',
         message: "Indiacte shape color (keyword or hexidecimal number)",
-        name: 'shape-color',
+        name: 'shapeColor',
         default: '',
         validate: function (answer) {
             if (answer.length < 1) {
@@ -67,6 +66,21 @@ const questions = [
             }
             return true;
         }
-    }
+    }]
+
+   
+
+   
+    function init() {
+        inquire.prompt(questions)
+        .then((answers) => {
+           // writeToFile("logo.svg", generateSVG({ ...answers }));
+          });
+        //.then(console.log('test 1 passed')) 
+        //.then((answers) => {
+        //writeToFile();
+        };
+      
+   init()     
     
-]
+ 
